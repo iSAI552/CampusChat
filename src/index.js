4e6a5b16a53c459d940d14d96af9401d897a8906
SAI <sai552kk@gmail.com>
2024-02-23 14:44:40 +0530

    Add express server configuration and error handling

--- src/index.js
+++ src/index.js
@@ -6,3 +6,16 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.error(`Error: ${error}`)
+        throw error
+    })
+    const runningPort = process.env.PORT || 8000
+    app.listen(runningPort, () => {
+        console.log(`Server is running on Port: ${runningPort}`)
+    })
+})
+.catch((error) => {
+    console.error(`MongoDB connection Failed !!!`, error);
+})
