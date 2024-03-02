64167c74a1e531765e4691d0c4e6bc92201b13f7
SAI <sai552kk@gmail.com>
2024-03-02 13:18:38 +0530

    Complete Schema of Media and Message

--- src/models/media.model.js
+++ src/models/media.model.js
@@ -2,7 +2,26 @@ import { Schema, model } from "mongoose";
 import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
 
 const mediaSchema = new Schema({
-
+    fileName: {
+        type: String,
+        required: true
+    },
+    fileType: {
+        type: String,
+        required: true,
+    },
+    fileSize: {
+        type: Number,
+        required: true,
+    },
+    fileURI: {
+        type: String, // Cloudinary link
+        required: true,
+    },
+    uploadedByUserId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    }
 }, {timestamps: true})
 
 mediaSchema.plugin(mongooseAggregatePaginate)
--- src/models/message.model.js
+++ src/models/message.model.js
@@ -1,6 +1,34 @@
 import { Schema, model } from "mongoose";
 
 const messageSchema = new Schema({
+    senderId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    receiverId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    content: {
+        type: String,
+    },
+    attachments: [{
+        url: {
+            type: String,
+            required: true
+        },
+        contentType: {
+            type: String,
+            required: true
+        }
+    }],
+    seen: {
+        type: Boolean,
+        default: false,
+    }
+
 
 }, { timestamps: true })
 
