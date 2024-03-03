d92ffbc9a1333b22da1ad70c52e0825c0e2a1747
SAI <sai552kk@gmail.com>
2024-03-03 14:47:04 +0530

    Complete the Notification Schema

--- src/models/notification.model.js
+++ src/models/notification.model.js
@@ -1,7 +1,26 @@
 import { Schema, model } from "mongoose";
 
 const notificationSchema = new Schema({
-
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    seen: {
+        type: Boolean,
+        default: false,
+    },
+    notificationType: {
+        type: String,
+        required: true,
+    },
+    
 }, { timestamps: true })
+
+// make a function that calculates sum of n integers
 
 export const Notification = model("Notification", notificationSchema)
