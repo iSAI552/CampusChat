b8026d04df846ec12217f88718354726e002bd4f
SAI <sai552kk@gmail.com>
2024-03-19 17:16:36 +0530

    Add Tailwind and container component

--- frontend/CampusChat/src/App.jsx
+++ frontend/CampusChat/src/App.jsx
@@ -1,5 +1,4 @@
 // import { useState } from 'react'
-
 import './App.css'
 
 function App() {
@@ -7,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>hello</h1>
+      <h1 className='text-3xl font-bold underline'>Hello world</h1>
     </>
   )
 }
